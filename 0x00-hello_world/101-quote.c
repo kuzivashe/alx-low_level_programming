9aa28c395849c764374f352236898f76e40943ce
Kuzivashe <kuzibaye@gmail.com>
2022-09-08 13:59:43 -0700

    UNIX is basically an operating system, but you have to be a genius to understand its simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * main -Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void) 
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58, 1, stderr);
+
+	return (0);
+
+}
