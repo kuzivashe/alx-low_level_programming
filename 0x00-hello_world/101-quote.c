af3da387bbd2eba1e0a1cdd3cedb9fb6320c4170
Kuzivashe <kuzibaye@gmail.com>
2022-09-08 14:14:53 -0700

    UNIX is basically an operating system, but you have to be a genius to understand its simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,16 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include <unistd.h>
 
 /**
   * main -Entry point
   *
-  * Return: Always 0 (Success)
+  * Return: Always 1 (Success)
   */
 int main(void) 
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58, 1, stderr);
+	char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	return (0);
+	write(2, x, 58);
+
+	return (1);
 
 }
