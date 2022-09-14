cde8fa273c42f03870f9470535888de9a08c8f0d
Kuzivashe <kuzibaye@gmail.com>
2022-09-13 17:30:08 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - print alphabet 10 times
+  *
+  * Return: 0 Awalys
+  */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			_putchar(i);
+		}
+		_putchar(c);
+	}
+_putchar('\n');
+return (0);
+}
