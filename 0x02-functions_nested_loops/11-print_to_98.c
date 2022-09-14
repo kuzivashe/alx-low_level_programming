ef19ea845579e2eb575c4f952594f7a7b1d462a0
Kuzivashe <kuzibaye@gmail.com>
2022-09-13 20:55:40 -0700

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,14 @@ void print_to_98(int n)
 	{
 		for (; n > 98; n--)
 		{
-			printf("%d\n", n);
+			printf("%d, \n", n);
 		}
 	}
 	else if (n < 98)
 	{
 		for (; n < 98; n++)
 		{
-			printf("%d\n", n);
+			printf("%d, \n", n);
 		}
 	}
 	printf("%d\n", n);
