459e161a1353810513f45236bd96023112b72d0f
Kuzivashe <kuzibaye@gmail.com>
2022-09-15 10:21:30 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - do not print all the numbers
+ *
+ * @c: input numbers
+ *
+ * Return: all numbers between 0 and 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar(x);
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@
 1. Write a function that checks for a digit (0 through 9). Run man isdigit to learn more.
 2. Write a function that multiplies 2 integers.
 3. Write a function that prints the numbers from 0 to 9, followed by a new line.
+4. Write a function that prints numbers from 0 to 9 except 2 and 4 followed by a new line.
