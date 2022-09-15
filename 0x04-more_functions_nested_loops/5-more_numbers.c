092d73786b3c1f0d8fd1dd74194d3e8cf6e55f8e
Kuzivashe <kuzibaye@gmail.com>
2022-09-15 11:24:40 -0700

    repeat numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - print the same numbers multiple times
+ *
+ * @i: numbers from 0 to 14
+ * @j: repeating the loop 10 times
+ *
+ * Return: repeated numbers
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + 48);
+			}
+  			_putchar((i % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
