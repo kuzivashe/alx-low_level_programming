dd600092d8d4e05eb38731d39b9bf2d8d169c394
Kuzivashe <kuzibaye@gmail.com>
2022-09-15 04:37:20 -0700

    Leap year

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -29,23 +29,18 @@ int convert_day(int month, int day)
 		case 7:
 			day = 181 + day;
 			break;
-
 		case 8:
 			day = 212 + day;
 			break;
-
 		case 9:
 			day = 243 + day;
 			break;
-
 		case 10:
 			day = 273 + day;
 			break;
-
 		case 11:
 			day = 304 + day;
 			break;
-
 		case 12:
 			day = 334 + day;
 			break;
