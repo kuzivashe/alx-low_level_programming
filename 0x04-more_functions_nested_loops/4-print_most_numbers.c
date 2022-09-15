2c2c59cdf9ec6c3ecdbf4714c549c233f9508765
Kuzivashe <kuzibaye@gmail.com>
2022-09-15 10:25:36 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,11 @@
  */
 void print_most_numbers(void)
 {
-	int x;
+	char x;
 
-	for (x = 0; x <= 9; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
-		if (x != 2 && x != 4)
+		if (x != '2' && x != '4')
 		{
 			_putchar(x);
 		}
