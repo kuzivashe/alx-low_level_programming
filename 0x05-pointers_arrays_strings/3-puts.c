f5c211d337eda88f440f83cbcbb45f76cc91f4d4
Kuzivashe <kuzibaye@gmail.com>
2022-09-19 15:09:00 -0700

    prints string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#inclde "main.h"
+
+/**
+* _puts - prints a string
+* @str: string pointer
+* Return: nothing
+*/
+void _puts(char *str)
+{
+	int word = 0;
+
+	while (word >= 0)
+	{
+		if (str[word] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[word]);
+		word++;
+	}
+}
