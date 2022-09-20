4ff9344089cc3bbf7fd4cd53248387ab8576f46d
Kuzivashe <kuzibaye@gmail.com>
2022-09-19 17:18:33 -0700

    half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,12 +17,20 @@ void puts_half(char *str)
 			_putchar('\n');
 			break;
 		}
-		if (i % 2 != 0)
-		{
-			n = _strlen(str[i] - 1) / 2;
-			_putchar(n);
-		}
 		i++;
 	}
+	if (i % 2 == 1)
+	{
+		n = i / 2;
+	}
+	else
+	{
+		n = (i - 1) / 2;
+	}
+	for (n++, n < i; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
 }
 
