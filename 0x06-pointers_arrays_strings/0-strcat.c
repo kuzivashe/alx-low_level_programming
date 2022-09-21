0ce14cce7c7a2d5e836ece33fd273f696d68f9aa
Kuzivashe <kuzibaye@gmail.com>
2022-09-21 04:58:20 -0700

    join those strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strcat - concatenate strings dest to src
+* @dest: destination character
+* @src: source character
+* Return: a pointer to the resulting string dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i = n = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		continue;
+	}
+	while (n >= 0)
+	{
+		*(dest + i) = *(src + n);
+		if (*(src + n) == '\0')
+		{
+			break;
+		}
+		i++;
+		n++;
+	}
+	return (dest);
+}
