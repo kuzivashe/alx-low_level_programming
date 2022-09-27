290fc2cd25d29e688017dba013fbced685b1dd32
Kuzivashe <kuzibaye@gmail.com>
2022-09-26 20:26:20 -0700

    searches a string for any set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,9 +13,9 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; *(accept + i) != '\0'; j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (*(s + i) == *(accept + 1))
+			if (*(s + i) == *(accept + j))
 			{
 				return (s + i);
 			}
