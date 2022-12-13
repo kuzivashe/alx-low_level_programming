545e7703b6c1d7694e430a5eba8da4d4b9cd22fa
Kuzivashe <kuzibaye@gmail.com>
2022-12-12 17:56:25 -0800

    UPDATE: dynamic library

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int _strlen(char *s);
+
 /**
   * main - checks code
   *
@@ -8,5 +10,19 @@
 int main(void)
 {
 	printf("%d\n", _strlen("My Dyn Lib"));
-	return (EXIT_SUCCESS);
+	return 0;
+}
+
+/**
+  * _strlen - length of string
+  * @s: string input
+  * Return - length of string
+  */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
 }
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 int _islower(int c);
