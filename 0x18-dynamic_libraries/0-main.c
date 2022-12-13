b057cde97b7dde5a662314f65240efd40fa05043
Kuzivashe <kuzibaye@gmail.com>
2022-12-12 16:47:21 -0800

    dynamic library

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * main - checks code
+  *
+  * Return: Always EXIT_SUCCESS.
+  */
+int main(void)
+{
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (EXIT_SUCCESS);
+}
