78f45e72467d7a6f9099d91fc2c6073c37c39236
Kuzivashe <kuzibaye@gmail.com>
2023-02-27 23:48:07 -0800

    latest updates

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_delete - free the entire hash table
+  * @ht: hash table to be free
+  * Return: nothing
+  */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *aux = NULL, *temp = NULL;
+	unsigned long int i;
+
+	if(ht == NULL)
+		return;
+
+	for(i = 0; i < ht->size; i++)
+	{
+		if(ht->array[i] != NULL)
+		{
+			aux = ht->array[i];
+			while(aux != NULL)
+			{
+				temp = aux;
+				aux = aux->next;
+				free(temp->key);
+				free(temp->value);
+				free(temp);
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
